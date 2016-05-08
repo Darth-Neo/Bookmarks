dadef8f636e31a3255b6237c1fcc9418762607bb
DarthNeo <morrisspid.james@hotmail.com>
2016-05-08 15:27:52 -0400

    3 Happy Mother's Day

--- ExportBookmarks.py
+++ ExportBookmarks.py
@@ -8,10 +8,16 @@ logger.setLevel(INFO)
 if __name__ == u"__main__":
 
     listFile = os.getcwd() + os.sep + u"run" + os.sep + u"folders.pl"
+    fileBookmarks = os.getcwd() + os.sep + u"run" + os.sep + u"bookmarks.pl"
     pl = loadList(listFile)
 
     folders, bookmarks = dumpCollection(pl)
 
+    listBookmarks = list()
     for link in bookmarks:
         if link[:4] == u"http":
+            listBookmarks.append(link)
             logger.info(u"%s" % link)
+
+    saveList(listBookmarks, fileBookmarks)
+
